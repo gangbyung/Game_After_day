68b8059b0b50b25dc6eaafc277807da3aa0be5b3
강병구 <a01066420995@gmail.com>
2024-09-22 22:05:45 +0900

    sound

--- Assets/Script/Manager/SoundManager.cs
+++ Assets/Script/Manager/SoundManager.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
-    public static SoundManager instance;  // 싱글톤 인스턴스
-    public AudioClip[] soundClips;        // 사운드 클립 배열
-    private AudioSource[] audioSources;   // 여러 AudioSource 배열
+    public static SoundManager instance;       // 싱글톤 인스턴스
+    public AudioClip[] soundClips;             // 사운드 클립 배열
+    private AudioSource[] audioSources;        // 여러 AudioSource 배열
+    public AudioMixer audioMixer;              // AudioMixer 참조
+    public AudioMixerGroup masterGroup;        // Master AudioMixerGroup 참조
+
+    public Slider volumeSlider;
 
     void Awake()
     {
@@ -22,14 +28,24 @@ public class SoundManager : MonoBehaviour
 
     void Start()
     {
+        // 슬라이더 초기값 설정
+        volumeSlider.value = 1.0f;
+
         // AudioSource 배열 초기화
         audioSources = new AudioSource[soundClips.Length];
+
+        // 슬라이더 값 변경 이벤트 추가
+        volumeSlider.onValueChanged.AddListener(SetVolume);
 
         // AudioSource 컴포넌트 추가 및 설정
         for (int i = 0; i < soundClips.Length; i++)
         {
-            audioSources[i] = gameObject.AddComponent<AudioSource>(); // AudioSource 추가
-            audioSources[i].clip = soundClips[i];                     // 사운드 클립 할당
+            // AudioSource 추가
+            audioSources[i] = gameObject.AddComponent<AudioSource>();
+            // 사운드 클립 할당
+            audioSources[i].clip = soundClips[i];
+            // AudioSource의 Output을 Master 그룹으로 설정
+            audioSources[i].outputAudioMixerGroup = masterGroup;
         }
     }
 
@@ -62,6 +78,20 @@ public class SoundManager : MonoBehaviour
         else
         {
             Debug.LogWarning("사운드 인덱스 벗어남.");
+        }
+    }
+
+    // 볼륨 설정 함수
+    public void SetVolume(float volume)
+    {
+        Debug.Log("Volume: " + volume);  // 볼륨 값 출력
+        if (volume <= 0.0001f)
+        {
+            audioMixer.SetFloat("Master", -80f);  // 음소거
+        }
+        else
+        {
+            audioMixer.SetFloat("Master", Mathf.Log10(volume) * 20);
         }
     }
 }
